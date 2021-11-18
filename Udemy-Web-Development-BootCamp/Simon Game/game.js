84afeb8192f88772ae424d3cb47818964245a10e
nohhow <xksrma97@gmail.com>
2021-11-18 12:50:27 +0900

    Simon Game step 4

--- Udemy-Web-Development-BootCamp/Simon Game/game.js
+++ Udemy-Web-Development-BootCamp/Simon Game/game.js
@@ -1,5 +1,6 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
 function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4);
@@ -7,4 +8,14 @@ function nextSequence(){
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
+
+  $('#'+randomChosenColour).animate({opacity:"0.5"}, 100);
+  $('#'+randomChosenColour).animate({opacity:"1"}, 100);
+  const audio = new Audio("sounds/"+randomChosenColour+".mp3");
+  audio.play();
 }
+
+$('.btn').click(function(){
+  var userChosenColour = $(this).attr('id');
+  userClickedPattern.push(userChosenColour);
+});
