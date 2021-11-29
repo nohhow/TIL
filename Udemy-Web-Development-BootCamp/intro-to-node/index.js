a88e8ce719e1bae7110142cb768fd3acb6a974fa
nohhow <xksrma97@gmail.com>
2021-11-30 03:35:55 +0900

    hero vs villains

--- Udemy-Web-Development-BootCamp/intro-to-node/index.js
+++ Udemy-Web-Development-BootCamp/intro-to-node/index.js
@@ -1,6 +1,9 @@
 //jshint esversion:6
 
+var superheroes = require("superheroes");
 var supervillains = require("supervillains");
 
+var mySuperheroName = superheroes.random();
 var mySupervillainsName = supervillains.random();
-console.log(mySupervillainsName);
+
+console.log(mySuperheroName + ' vs ' + mySupervillainsName);
