24b33d00ba1f19168b878981ac73af3cdf4a0d3d
nohhow <xksrma97@gmail.com>
2021-12-01 03:29:08 +0900

    update first server

--- Udemy-Web-Development-BootCamp/my-express-server/server.js
+++ Udemy-Web-Development-BootCamp/my-express-server/server.js
@@ -7,6 +7,10 @@ app.get("/", function(request, response){
   response.send("<h1>Hello</h1>");
 });
 
+app.get("/about", function(req, res){
+  res.send("<h1>About Me</h1><ul><li>한남대학교 졸업</li><li>제이탑미래기술 재직</li><li>루트임팩트 베이스캠프 수료</li></ul>")
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
