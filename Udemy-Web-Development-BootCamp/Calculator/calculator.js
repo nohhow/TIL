644119ab0412d92885879c6184252838469ed755
nohhow <xksrma97@gmail.com>
2021-12-03 13:56:39 +0900

    BMI Calculator

--- Udemy-Web-Development-BootCamp/Calculator/calculator.js
+++ Udemy-Web-Development-BootCamp/Calculator/calculator.js
@@ -10,10 +10,6 @@ app.get("/", function(req, res){
   res.sendFile(__dirname+"/index.html");
 });
 
-app.get("/divide", function(req, res){
-  res.send("divide session");
-});
-
 app.post("/", function(req, res){
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
@@ -21,6 +17,19 @@ app.post("/", function(req, res){
   var result = num1 + num2;
 
   res.send("The result of the calculation is " + result);
+});
+
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname+"/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var result = weight / (height**2);
+
+  res.send("The result of the BMI calculation is " + result);
 });
 
 app.listen(3000, function(){
