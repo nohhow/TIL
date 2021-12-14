28bf1f1a66807a34bd54e51f0194d23faf3be8b7
nohhow <xksrma97@gmail.com>
2021-12-15 02:41:24 +0900

    newsletter signup complete

--- Udemy-Web-Development-BootCamp/Newsletter-Signup/app.js
+++ Udemy-Web-Development-BootCamp/Newsletter-Signup/app.js
@@ -3,8 +3,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
+const client = require('@mailchimp/mailchimp_marketing');
+
 
 const app = express();
+
+client.setConfig({apiKey:"0b6232c5445d21cdcd8a180334a03967-uas20", server:"us20",});
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -14,13 +19,45 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-  var firstName = req.body.FirstName;
-  var lastName = req.body.LastName;
-  var email = req.body.Email;
+  const firstName = req.body.FirstName;
+  const lastName = req.body.LastName;
+  const email = req.body.Email;
 
-  console.log("Full Name : " + lastName + " " + firstName + " Eamil : " + email);
+  console.log(firstName, lastName, email);
+
+  const subscribingUser = {firstName: firstName, lastName: lastName, email: email};
+
+  const run = async () => {
+    try{
+    const response = await client.lists.addListMember("b0737ca45c", {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName
+      }
+    });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      console.log(err.status);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+
+  run();
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
 });
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
 });
+
+// api Key
+// 0b6232c5445d21cdcd8a180334a03967-us20
+
+//list id
+// b0737ca45c
