7628eefb21b0252596ac85b234596b18cb413b30
nohhow <xksrma97@gmail.com>
2021-12-14 19:18:52 +0900

    newsletter prep work

--- Udemy-Web-Development-BootCamp/Newsletter-Signup/app.js
+++ Udemy-Web-Development-BootCamp/Newsletter-Signup/app.js
@@ -1,9 +1,26 @@
+//jshint esversion: 6
+
 const express = require('express');
-const bodyParser = require('bodyParser');
-const request = reuquire('request');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  var firstName = req.body.FirstName;
+  var lastName = req.body.LastName;
+  var email = req.body.Email;
+
+  console.log("Full Name : " + lastName + " " + firstName + " Eamil : " + email);
+});
+
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
-})
+});
